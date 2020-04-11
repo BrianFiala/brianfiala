059bdfe46858423df01befcb942df1685dad90eb
Brian Fiala <brian_fiala@homedepot.com>
2020-04-10 22:06:10 -0500

    started setting a theme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Body from './components/Body'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import 'typeface-roboto'
-import './index.css'
+import './index.scss'
 
 export default function App() {
   return (
--- src/themes/index.js
+++ src/themes/index.js
@@ -1,6 +1,22 @@
 import { createMuiTheme } from '@material-ui/core/styles'
+// import red from '@material-ui/core/colors/red'
+// import green from '@material-ui/core/colors/green'
 
 const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#F5F5F5',
+      main: '#9E9E9E',
+      dark: '#616161'
+      // contrastText: 
+    },
+    secondary: {
+      light: '#F5F5F5',
+      main: '#9E9E9E',
+      dark: '#616161'
+      // contrastText: 
+    }
+  }
 })
 
 export default theme
